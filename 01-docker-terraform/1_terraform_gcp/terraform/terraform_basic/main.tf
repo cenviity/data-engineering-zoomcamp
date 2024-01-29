0f23e7b5a506a3000e67caf84dc013a67f18ec15
Vincent Ging Ho Yim <cenviity@gmail.com>
2024-01-29 17:33:04 +1100

    Add Google Cloud Storage bucket as resource

--- 01-docker-terraform/1_terraform_gcp/terraform/terraform_basic/main.tf
+++ 01-docker-terraform/1_terraform_gcp/terraform/terraform_basic/main.tf
@@ -11,3 +11,18 @@ provider "google" {
   project = "even-ally-412601"
   region  = "us-central1"
 }
+
+resource "google_storage_bucket" "demo-bucket" {
+  name          = "even-ally-412601-terra-bucket"
+  location      = "US"
+  force_destroy = true
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
+}
