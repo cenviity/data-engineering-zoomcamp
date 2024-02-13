ecc7b13353156d79b6c3bfbe70eb568220b15251
Vincent Ging Ho Yim <cenviity@gmail.com>
2024-02-14 08:33:14 +1100

    Update pipeline to load all 2022 Parquet files

--- 03-data-warehouse/green-taxi-trips-2022-dlt/green_taxi_trips_2022_dlt/load_taxi_data.py
+++ 03-data-warehouse/green-taxi-trips-2022-dlt/green_taxi_trips_2022_dlt/load_taxi_data.py
@@ -4,10 +4,16 @@ import dlt
 import pandas as pd
 from dlt.sources.helpers import requests
 
-url = "https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2022-01.parquet"
 
-response = requests.get(url, stream=True)
-response.raise_for_status()
+@dlt.resource
+def load_parquet_files():
+    for month in range(1, 13):
+        url = f"https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2022-{month:02}.parquet"
+        response = requests.get(url, stream=True)
+        response.raise_for_status()
+        data = pd.read_parquet(io.BytesIO(response.content))
+        yield data
+
 
 pipeline = dlt.pipeline(
     pipeline_name="load_taxi_data",
@@ -16,7 +22,7 @@ pipeline = dlt.pipeline(
 )
 
 load_info = pipeline.run(
-    pd.read_parquet(io.BytesIO(response.content)),
+    load_parquet_files(),
     loader_file_format="parquet",
     table_name="trips",
     write_disposition="replace",
