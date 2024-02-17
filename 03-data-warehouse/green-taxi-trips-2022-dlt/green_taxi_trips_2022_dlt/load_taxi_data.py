8efcbe5eb25c2bdac214390366cec5a15e57b18b
Vincent Ging Ho Yim <cenviity@gmail.com>
2024-02-17 17:00:30 +1100

    Fix logic to load as separate files instead of a single file

--- 03-data-warehouse/green-taxi-trips-2022-dlt/green_taxi_trips_2022_dlt/load_taxi_data.py
+++ 03-data-warehouse/green-taxi-trips-2022-dlt/green_taxi_trips_2022_dlt/load_taxi_data.py
@@ -1,40 +1,49 @@
 import io
+import logging
 
 import dlt
 import pandas as pd
+from dlt.destinations import filesystem
 from dlt.sources.credentials import GcpServiceAccountCredentials
 from dlt.sources.helpers import requests
 
+logging_level = logging.INFO
+logging.basicConfig(level=logging_level)
 
-@dlt.resource
-def load_parquet_files():
-    for month in range(1, 13):
-        url = f"https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2022-{month:02}.parquet"
+for month in range(1, 13):
+    _month = f"{month:02}"
+
+    @dlt.resource
+    def load_parquet_files():
+        url = (
+            "https://d37ci6vzurychx.cloudfront.net/trip-data/"
+            f"green_tripdata_2022-{_month}.parquet"
+        )
         response = requests.get(url, stream=True)
         response.raise_for_status()
         data = pd.read_parquet(io.BytesIO(response.content))
         yield data
 
+    credentials = GcpServiceAccountCredentials()
+    with open(".dlt/gcp-secrets.json", "r") as f:
+        native_value = f.read()
+    credentials.parse_native_representation(native_value)
 
-credentials = GcpServiceAccountCredentials()
-with open(".dlt/gcp-secrets.json", "r") as f:
-    native_value = f.read()
-credentials.parse_native_representation(native_value)
-
-pipeline = dlt.pipeline(
-    pipeline_name="load_taxi_data",
-    destination=dlt.destinations.filesystem(
+    destination = filesystem(
         bucket_url="gs://de-zoomcamp-module-3-homework-vincent-yim-dlt",
         credentials=credentials,
-    ),
-    dataset_name="green_taxi_trips_2022",
-)
+        layout=f"{{table_name}}/green_tripdata_2022-{_month}",
+    )
 
-load_info = pipeline.run(
-    load_parquet_files(),
-    loader_file_format="parquet",
-    table_name="trips",
-    write_disposition="replace",
-)
+    pipeline = dlt.pipeline(
+        pipeline_name="load_taxi_data",
+        destination=destination,
+        dataset_name="green_taxi_trips_2022",
+    )
 
-print(load_info)
+    load_info = pipeline.run(
+        load_parquet_files,
+        loader_file_format="parquet",
+        table_name="trips",
+    )
+    logging.info(load_info)
