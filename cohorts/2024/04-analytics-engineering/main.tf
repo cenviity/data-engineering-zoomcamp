507ae30b3209e93e22f9edf4dbbbb27bca2da4e4
Vincent Ging Ho Yim <28189684+cenviity@users.noreply.github.com>
2024-02-25 01:35:26 +1100

    Remove GCS bucket config since only BigQuery is needed for module 4

--- cohorts/2024/04-analytics-engineering/main.tf
+++ cohorts/2024/04-analytics-engineering/main.tf
@@ -13,21 +13,6 @@ provider "google" {
   region      = var.region
 }
 
-resource "google_storage_bucket" "de-zoomcamp" {
-  name          = var.gcs_bucket_name
-  location      = var.location
-  force_destroy = true
-
-  lifecycle_rule {
-    condition {
-      age = 1
-    }
-    action {
-      type = "AbortIncompleteMultipartUpload"
-    }
-  }
-}
-
 resource "google_bigquery_dataset" "ny_taxi_trips" {
   dataset_id = var.bq_dataset_name
   location   = var.location
