7b9b7468ce7dede97df9af18f7e374ef6c534efb
Vincent Ging Ho Yim <28189684+cenviity@users.noreply.github.com>
2024-02-29 21:28:25 +1100

    Update staging tables using code from lesson
    https://github.com/DataTalksClub/data-engineering-zoomcamp/blob/9d62b2cc612b4174bf8d5353d92de1ee3d62ed01/04-analytics-engineering/taxi_rides_ny/models/staging/stg_green_tripdata.sql
    https://github.com/DataTalksClub/data-engineering-zoomcamp/blob/9d62b2cc612b4174bf8d5353d92de1ee3d62ed01/04-analytics-engineering/taxi_rides_ny/models/staging/stg_yellow_tripdata.sql

--- cohorts/2024/04-analytics-engineering/dbt-taxi-rides-ny/models/staging/stg_yellow_taxi_trips.sql
+++ cohorts/2024/04-analytics-engineering/dbt-taxi-rides-ny/models/staging/stg_yellow_taxi_trips.sql
@@ -10,31 +10,31 @@ tripdata as (
 
     select
         *,
-        row_number() over (partition by vendor_id, lpep_pickup_datetime) as rn
+        row_number() over (partition by vendor_id, tpep_pickup_datetime) as rn
 
-    from {{ source('staging','green_taxi_trips') }}
+    from {{ source('staging','yellow_taxi_trips') }}
 
     where vendor_id is not null
-
 )
 
 select
     -- identifiers
-    {{ dbt_utils.generate_surrogate_key(['vendor_id', 'lpep_pickup_datetime']) }} as tripid,
+    {{ dbt_utils.generate_surrogate_key(['vendor_id', 'tpep_pickup_datetime']) }} as tripid,
     {{ dbt.safe_cast("vendor_id", api.Column.translate_type("integer")) }} as vendorid,
     {{ dbt.safe_cast("ratecode_id", api.Column.translate_type("integer")) }} as ratecodeid,
     {{ dbt.safe_cast("pu_location_id", api.Column.translate_type("integer")) }} as pickup_locationid,
     {{ dbt.safe_cast("do_location_id", api.Column.translate_type("integer")) }} as dropoff_locationid,
 
     -- timestamps
-    cast(lpep_pickup_datetime as timestamp) as pickup_datetime,
-    cast(lpep_dropoff_datetime as timestamp) as dropoff_datetime,
+    cast(tpep_pickup_datetime as timestamp) as pickup_datetime,
+    cast(tpep_dropoff_datetime as timestamp) as dropoff_datetime,
 
     -- trip info
     store_and_fwd_flag,
     {{ dbt.safe_cast("passenger_count", api.Column.translate_type("integer")) }} as passenger_count,
     cast(trip_distance as numeric) as trip_distance,
-    {{ dbt.safe_cast("trip_type", api.Column.translate_type("integer")) }} as trip_type,
+    -- yellow cabs are always street-hail
+    1 as trip_type,
 
     -- payment info
     cast(fare_amount as numeric) as fare_amount,
@@ -42,11 +42,11 @@ select
     cast(mta_tax as numeric) as mta_tax,
     cast(tip_amount as numeric) as tip_amount,
     cast(tolls_amount as numeric) as tolls_amount,
-    cast(ehail_fee as numeric) as ehail_fee,
+    cast(0 as numeric) as ehail_fee,
     cast(improvement_surcharge as numeric) as improvement_surcharge,
     cast(total_amount as numeric) as total_amount,
     coalesce({{ dbt.safe_cast("payment_type", api.Column.translate_type("integer")) }}, 0) as payment_type,
-    {{ get_payment_type_description("payment_type") }} as payment_type_description
+    {{ get_payment_type_description('payment_type') }} as payment_type_description
 
 from tripdata
 
